8544b4a1f25ab417dc7ba4d7e8eb225eecff9283
Alexey-Popov1410 <61269622+Alexey-Popov1410@users.noreply.github.com>
2020-02-27 18:53:42 +0300

    Initial commit

---
+++ src/Lesson04/Interval.java
@@ -0,0 +1,44 @@
+package Lesson04;
+
+public class Interval {
+    int second;
+    int minute;
+    int hour;
+
+
+     Interval(int hour, int minute, int second){
+        this.second = second;
+        this.minute = minute;
+        this.hour = hour;
+        hour *= 3600;
+        minute *= 60;
+        second = hour + minute + second;
+        if (hour < 0 || minute < 0 || second < 0){
+            System.out.println("Время не может быть отрицательным");
+        } else {
+            System.out.println("Общее число секунд : " + second);
+        }
+    }
+
+
+    Interval (int second){
+        this.second = second;
+        hour = second / 3600;
+        minute = (second -(hour*3600))/60;
+        second = ((second - (hour*3600)) - (minute * 60));
+        if (second < 0){
+            System.out.println("Время не может быть отрицательным");
+        } else {
+            System.out.println("Часы . Минуты . Секунды : " + hour + "." + minute + "." + second);
+        }
+    }
+
+
+}
+class time{
+    public static void main(String[] args){
+        Interval time = new Interval(1,10,0);
+
+    }
+}
+
