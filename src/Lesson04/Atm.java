e9f411fce2e638e9f3efff91b6c238268d9dfe4d
Alexey-Popov1410 <61269622+Alexey-Popov1410@users.noreply.github.com>
2020-03-17 11:33:59 +0300

    Initial commit

--- src/Lesson04/Atm.java
+++ src/Lesson04/Atm.java
@@ -1,36 +1,56 @@
 package Lesson04;
 
 public class Atm {
-    private int cash20;
-    private int cash50;
-    private int cash100;
-    private int allCash;
+    private static final int RATE_20 = 20;
+    private static final int RATE_50 = 50;
+    private static final int RATE_100 = 100;
+    private int rate20Count;
+    private int rate50Count;
+    private int rate100Count;
 
-    public static void main(String[] args){
-        Atm one = new Atm(7,2,5);
-
-
+    public Atm(int rate20Count, int rate50Count, int rate100Count) {
+        this.rate20Count = rate20Count;
+        this.rate50Count = rate50Count;
+        this.rate100Count = rate100Count;
     }
 
-    public Atm(int cash20, int cash50, int cash100){
-        this.cash20 = 0;
-        this.cash50 = 0;
-        this.cash100 = 0;
-    }
-    public static void addCash(int cash20, int cash50, int cash100, int addCash20, int addCash50, int addCash100){
-        cash20 += addCash20;
-        cash50 += addCash50;
-        cash100 += addCash100;
-        int allAddCash = addCash20 * 20 + addCash50 * 50 + addCash100 * 100;
-        int allCash = cash20 * 20 + cash50 * 50 + cash100 * 100;
-
+    public void addRate20(int rate20Count) {
+        this.rate20Count += rate20Count;
     }
 
-    public static boolean takeAwayCash(int sum, int cash20, int cash50, int cash100){
-        if(sum > cash20 * 20 + cash50 * 50 + cash100 * 100){
-            sum -= cash20 * 20 + cash50 * 50 + cash100 * 100;
-            return true;
-        }else return false;
+    public void addRate50(int rate50Count) {
+        this.rate50Count += rate50Count;
+    }
+
+    public void addRate100(int rate100Count) {
+        this.rate100Count += rate100Count;
+    }
+
+    public boolean takeOffMoney(int takeMoney) {
+        int rate20 = rate20Count;
+        int rate50 = rate50Count;
+        int rate100 = rate100Count;
+        while (takeMoney != 0) {
+            if (takeMoney >= RATE_100 && rate100Count > 0 && (takeMoney / 10) % 2 == 0) {
+                takeMoney -= RATE_100;
+                rate100Count--;
+            } else if (takeMoney >= RATE_50 && rate50Count > 0 && ((takeMoney / 10) % 2 != 0 || takeMoney % 50 == 0)) {
+                takeMoney -= RATE_50;
+                rate50Count--;
+            } else if (takeMoney >= RATE_20 && rate20Count > 0) {
+                takeMoney -= RATE_20;
+                rate20Count--;
+            } else {
+                System.out.println("Ошибка выдачи");
+                rate20Count = rate20;
+                rate50Count = rate50;
+                rate100Count = rate100;
+                return false;
+            }
+
+        }
+        System.out.println("Были выданы купюры номиналом\n100 : " + (rate100 - rate100Count) + ";\n 50 : " + (rate50 - rate50Count) + ";\n 20 : " + (rate20 - rate20Count) + ".");
+        return true;
     }
 }
 
