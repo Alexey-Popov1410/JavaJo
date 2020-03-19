96ad88201288490a18857334ed761f54676ed8a1
Alexey-Popov1410 <61269622+Alexey-Popov1410@users.noreply.github.com>
2020-03-19 12:08:13 +0300

    Initial commit

--- src/Lesson14/Regex3.java
+++ src/Lesson14/Regex3.java
@@ -6,11 +6,11 @@ public class Regex3 {
     }
 
     public static void main(String[] args) {
-        String s3 = "<pom class=\"header\"> \n" +
-                "   <plum class=\"subTitle\">\n" +
-                "    <triam class=\"packageLabelInType\">Package\n" +
-                "    <wow href=\"../../../org/jsoup/nodes/package-summary.html\">org.jsoup.nodes\n" +
-                "   <h2 title=\"Class Element\" class=\"title\">Class  \n";
+        String s3 = " <pom class=\"header\"> \n" +
+                " <plum class=\"subTitle\">\n" +
+                " <triam class=\"packageLabelInType\">Package\n" +
+                " <wow href=\"../../../org/jsoup/nodes/package-summary.html\">org.jsoup.nodes\n" +
+                " <h2 title=\"Class Element\" class=\"title\">Class  \n";
         System.out.println(replace(s3));
     }
 }
