84ecc35e23c5b60a57a2280f8109931ec1c4c0cd
Alexey-Popov1410 <61269622+Alexey-Popov1410@users.noreply.github.com>
2020-04-04 12:53:15 +0300

    Initial commit

--- src/Lesson15/Main.java
+++ src/Lesson15/Main.java
@@ -1,45 +1,133 @@
 package Lesson15;
 import java.io.*;
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        FileToConsole toConsole = null;
-        try (BufferedReader bf = new BufferedReader(new InputStreamReader(System.in))) {
-            toConsole = new FileToConsole("src" + File.separator + "Lesson15" + File.separator + "One.txt");
+        System.out.println(" Задание 1 ");
+        System.out.println(" Все слова построчно : ");
+        String a = "";
+        try (BufferedReader line = new BufferedReader(new FileReader("D://test//text.txt"))) {
+            while (true) {
+                try {
+                    if ((a = line.readLine()) == null) break;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(a);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        System.out.println(" Задание 2 ");
+        System.out.println(" Все слова начинающиеся с гласных : ");
+        try (BufferedReader line = new BufferedReader(new FileReader("D://test//text.txt"))) {
+            while ((a = line.readLine()) != null) {
+                String[] words = a.split("[\\s,.:!?]+");
+                Pattern pattern = Pattern.compile("^[aeyi]");
+                for (String word : words) {
+                    Matcher matcher = pattern.matcher(word.toLowerCase());
+                    if (matcher.find()) {
+                        System.out.println(word);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
 
-//        System.out.println(" Задача_1 ");
-//        toConsole.printAll();
+        System.out.println(" Задание 3 ");
+        System.out.println(" Последняя буква одного слова совпадает с первой буквой\n" +
+                "следующего слова : ");
+        try (BufferedReader line = new BufferedReader(new FileReader("D://test//text.txt"))) {
+            while ((a = line.readLine()) != null) {
+                String[] words = a.split("[\\s,.:!?]+");
+                for (int i = 0; i < words.length - 1; i++) {
+                    if (!words[i].isEmpty() && !words[i + 1].isEmpty()) {
+                        if (words[i].charAt(words[i].length() - 1) == words[i + 1].charAt(0)) {
+                            System.out.println(words[i] + " " + words[i + 1]);
+                        }
+                    }
+                }
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
 
+        System.out.println(" Задание 4 ");
+        System.out.println(" Наибольшее число цифр в стороке, идущих подряд : ");
+        try (BufferedReader line = new BufferedReader(new FileReader("D://test//text.txt"))) {
+            while (line.ready()) {
+                int length = 0;
+                Pattern pattern = Pattern.compile("\\d+");
+                Matcher matcher = pattern.matcher(line.readLine());
+                while (matcher.find()) {
+                    if (matcher.group().length() > length) {
+                        length = matcher.group().length();
+                        a = matcher.group();
+                    }
+                }
+                if(a != null) {System.out.println(a);}
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-//        System.out.println(" Задача_2 ");
-//        toConsole.wordsVowelsFirst();
+        System.out.println(" Задание 5 ");
+        System.out.println("  Распечатка\n" +
+                "чисел и их среднее арифметическое : ");
+        try (DataOutputStream line = new DataOutputStream(new FileOutputStream("D://test//text.txt"))) {
+            for (int i = 0; i < 20; i++) {
+                int d = (int) (256 + (Math.random() *100));
+                line.write(d);
+            }
+            line.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (DataInputStream line = new DataInputStream(new FileInputStream("D://test//text.txt"))) {
+            int sum = 0, i = 0;
+            System.out.println(" Прочитали числа: ");
+            while (line.available() > 0) {
+                int text = line.read();
+                System.out.print(text + " ");
+                sum += text;
+                i++;
+            }
+            System.out.println(" Cреднее значение : " + sum / i);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        System.out.println(" Задание 6 ");
+        System.out.println(" Список файлов и каталогов : ");
+        File file = new File("D:" + File.separator + "test");
+        listCatalog(file);
 
-//        System.out.println(" Задача_3 ");
-//        toConsole.lastFirstLetter();
+        System.out.println(" Задание 7 ");
+        try (BufferedReader line = new BufferedReader(new FileReader("D:" + File.separator
+                + "test" + File.separator + "text.java"));
+             BufferedWriter writer = new BufferedWriter(new FileWriter("D:" + File.separator
+                     + "test" + File.separator +  "text1.java"))) {
+            while ((a = line.readLine()) != null) {
+                StringBuilder b = new StringBuilder(a);
+                writer.write(String.valueOf(b.reverse()) + "\n");
+            }
 
-
-//        System.out.println(" Задача_4 ");
-//        toConsole.maxNumeralInLine();
-
-
-//        System.out.println(" Задача_5 ");
-//        BinaryFile wf = new BinaryFile("src" + File.separator + "Lesson15" + File.separator + "BinaryFile");
-//        wf.writeBinaryData();
-//        wf.readBinaryData();
-
-
-//        System.out.println(" Задача_6 ");
-//        ListDirectory listDirectory = new ListDirectory();
-//        listDirectory.listAll(new File("D:\\tests\\abc"));
-
-
-//        System.out.println(" Задача_7 ");
-//        ReverseJava reverseJava = new ReverseJava("src" + File.separator + "Lesson15" + File.separator + "Main.java", "src" + File.separator + "forLesson15" + File.separator + "ReversCod");
-//        reverseJava.revers();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void listCatalog(File file) {
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            assert files != null;
+            for (File s : files) {
+                System.out.println(s);
+            }
+        }
     }
 }
